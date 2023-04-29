75c170eac3cc9ceaac7092217dc29d6381bee030
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2023-04-29 23:39:41 +0300

    Create homepage

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import styles from './styles.module.scss'
+import data from '@/data/feed'
+import Masonry from '@/components/core/masonry'
+import QuestionAnswerCard from '@/components/custom/question-answer-card'
+import SuggestedUsers from '@/components/custom/suggested-users'
+import Footer from '@/components/custom/footer'
+
+const Page = () => {
+  return (
+    <div className={styles.container}>
+      <aside className={styles.peopleContainer}>
+        <div className={styles.people}>
+          <SuggestedUsers />
+          <Footer />
+        </div>
+      </aside>
+      <section className={styles.feed}>
+        <Masonry breakpointColumns={{ default: 2, '1280': 2, '1024': 1 }}>
+          {data.map((question) => (
+            <QuestionAnswerCard question={question} key={question.id} />
+          ))}
+        </Masonry>
+      </section>
+    </div>
+  )
+}
+
+export default Page
