67b38477360be31b1acc6718c6ca5881f516f99c
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2023-05-05 23:46:29 +0300

    Update QuestionAnswerCard with the reply feature

--- components/custom/question-answer-card/index.tsx
+++ components/custom/question-answer-card/index.tsx
@@ -1,64 +1,27 @@
 import React from 'react'
 
+import { QuestionType } from 'types'
 import styles from './question-answer-card.module.scss'
 import { Card, CardHeader, CardFooter } from '../../core/card'
-import UserAvatar from '../user-avatar'
-import { format } from 'timeago.js'
-import Link from 'next/link'
+import Answer from '../answer'
+import AnswerInput from '../answer-input'
 
-interface UserType {
-  name: string
-  username: string
-  avatar: string
-}
-
-interface AnswerType {
-  text: string
-  createdAt: number
-}
-
-interface QuestionAnswerCardProps {
-  question: {
-    id: number
-    to: UserType
-    text: string
-    answer: AnswerType
-    createdAt: number
-  }
-}
-
-const QuestionAnswerCard = ({ question }: QuestionAnswerCardProps) => {
+const QuestionAnswerCard = ({ question }: { question: QuestionType }) => {
   return (
     <Card>
       <CardHeader>
         <div className={styles.questionText}>{question.text}</div>
       </CardHeader>
       <CardFooter>
-        <Link href={`/${question.to.username}`}>
-          <UserAvatar
-            className={styles.avatar}
-            src={question.to.avatar}
-            width={36}
-            height={36}
+        {question.answer ? (
+          <Answer
+            recipient={question.to}
+            text={question.answer.text}
+            createdAt={question.createdAt}
           />
-        </Link>
-        <div className={styles.answer}>
-          <div className={styles.answerInfo}>
-            <Link href={`/${question.to.username}`}>
-              <span className={styles.name}>{question.to.name}</span>
-            </Link>
-            <div>
-              <Link href={`/${question.to.username}`}>
-                <span className={styles.username}>@{question.to.username}</span>
-              </Link>
-              <span className={styles.dividerDot}>·</span>
-              <time className={styles.timeAgo}>
-                {format(question.answer.createdAt, 'short')}
-              </time>
-            </div>
-          </div>
-          <div className={styles.answerText}>{question.answer.text}</div>
-        </div>
+        ) : (
+          <AnswerInput />
+        )}
       </CardFooter>
     </Card>
   )
