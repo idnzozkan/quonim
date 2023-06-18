0eca9eb47504891af13202d05a8e3de1d191a8c0
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2023-06-18 23:51:33 +0300

    Fix ref forwarding for the textarea component

--- components/core/textarea/index.tsx
+++ components/core/textarea/index.tsx
@@ -3,11 +3,21 @@ import clsx from 'clsx'
 
 import styles from './textarea.module.scss'
 
-const Textarea: React.FC<React.TextareaHTMLAttributes<HTMLTextAreaElement>> = ({
-  className,
-  ...props
-}) => {
-  return <textarea className={clsx(className, styles.textarea)} {...props} />
-}
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={clsx(className, styles.textarea)}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+
+Textarea.displayName = 'Textarea'
 
 export default Textarea
