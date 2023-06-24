18bcbad7efeef6cd651a25a7642a0db1c520378a
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2023-06-24 23:40:27 +0300

    Add support components

---
+++ components/support/icons/index.tsx
@@ -0,0 +1,34 @@
+import {
+  Compass,
+  Send,
+  MailQuestion,
+  MessageCircle,
+  Settings,
+  Users,
+  Reply,
+  Loader2,
+  Share2,
+  Save,
+  Trash2,
+  X,
+  Plus,
+  type Icon as IconType,
+} from 'lucide-react'
+
+export type Icon = IconType
+
+export const Icons = {
+  Explore: Compass,
+  Send,
+  Questions: MailQuestion,
+  Answers: MessageCircle,
+  Settings,
+  People: Users,
+  Reply,
+  Spinner: Loader2,
+  Share: Share2,
+  Save,
+  Trash: Trash2,
+  Delete: X,
+  Add: Plus,
+}
